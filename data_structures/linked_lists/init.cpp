b345119df27ff8ff1a9b825bc2eda1efa59281ea
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-07-23 04:01:38 +0600

    Update init.cpp

--- data_structures/linked_lists/init.cpp
+++ data_structures/linked_lists/init.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+  // suppose i would like to store some information of hospital about patient details
+  // hence, available info of a patient in a hospital is:
+  // 1. floor_num
+  // 2. room_num
+  // 3. cabin_num
+  // 4. patient_name
+  // 5. appoint_by
+  // 6. others
+  
   return 0;
 }
